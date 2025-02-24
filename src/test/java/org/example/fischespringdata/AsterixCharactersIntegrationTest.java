95f9a99da40813af6ce74d5599d4d750f098bb9d
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-24 12:29:33 +0100

    weird shit is still happening!!!! :((

--- src/test/java/org/example/fischespringdata/AsterixCharactersIntegrationTest.java
+++ src/test/java/org/example/fischespringdata/AsterixCharactersIntegrationTest.java
@@ -16,7 +16,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = FischeSpringDataApplication.class)
 @AutoConfigureMockMvc
-@Commit
 public class AsterixCharactersIntegrationTest
 {
     @Autowired
