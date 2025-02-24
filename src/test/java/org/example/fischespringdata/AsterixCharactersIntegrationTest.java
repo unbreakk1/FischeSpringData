eb0e6449101368daa787743aa76e9d5d17e2a71b
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-24 12:11:57 +0100

    add @Transactional 6 out of 6 fail 8)

--- src/test/java/org/example/fischespringdata/AsterixCharactersIntegrationTest.java
+++ src/test/java/org/example/fischespringdata/AsterixCharactersIntegrationTest.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = FischeSpringDataApplication.class)
 @AutoConfigureMockMvc
+@Transactional // Rolls back changes after tests are executed
 public class AsterixCharactersIntegrationTest
 {
 
